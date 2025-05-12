c4fe5e1223630f2bafacdb03e5f95dfd6e95a699
Nick Bolton <nick@symless.com>
2025-05-12 17:57:51 +0100

    Add debug logging for locked settings in ServerConfigDialog and SettingsDialog

--- src/lib/gui/dialogs/SettingsDialog.cpp
+++ src/lib/gui/dialogs/SettingsDialog.cpp
@@ -374,14 +374,17 @@ void SettingsDialog::updateControls()
 
   auto &locked = m_appConfig.settings().getLockedSettings();
   if (locked.contains("cryptoEnabled")) {
+    qDebug("locking tls");
     m_pCheckBoxEnableTls->setEnabled(false);
   }
   if (locked.contains("tlsCertPath")) {
+    qDebug("locking tls cert path");
     m_pLineEditTlsCertPath->setEnabled(false);
     m_pPushButtonTlsCertPath->setEnabled(false);
     m_pPushButtonTlsRegenCert->setEnabled(false);
   }
   if (locked.contains("tlsKeyLength")) {
+    qDebug("locking tls key length");
     m_pComboBoxTlsKeyLength->setEnabled(false);
   }
 }
