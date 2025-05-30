914441a6d53101cb82efe322e08a7df3e8681ee7
Nick Bolton <nick@symless.com>
2025-05-30 13:19:16 +0100

    Remove duplicate  subproject installation logic

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -261,12 +261,6 @@ class Dependencies:
 
         linux.run_command(command, check=True)
 
-        subprojects = self.config.get_os_subprojects()
-        if subprojects:
-            for subproject in subprojects:
-                deps = SubprojectDependencies(subproject)
-                deps.install()
-
 
 class SubprojectDependencies:
 
