e965748325a6ef83954fc101fb3910b91ea6957b
Nick Bolton <nick@symless.com>
2025-06-05 07:07:59 +0100

    Refactor main function and argument parsing in install_deps.py for improved clarity and maintainability

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -26,33 +26,11 @@ cmake_prefix_env_var = "CMAKE_PREFIX_PATH"
 
 
 def main():
-    is_ci = os.getenv("CI") is not None
-    if is_ci:
-        print("CI environment detected")
-
-    args = parse_args(is_ci)
-    if args.lock_file:
-        env.persist_lock_file(args.lock_file)
-
-    try:
-        run(args)
-    except Exception:
-        traceback.print_exc()
-        sys.exit(1)
-    finally:
-        if env.is_windows() and args.pause_on_exit:
-            # Allow the rest of the install to continue while sitting at the pause.
-            if args.lock_file:
-                env.remove_lock_file(args.lock_file)
-
-            # Useful on Windows, when elevated, Python is opened in a new window and closes
-            # immediately after the script finishes. This keeps the script window open so that
-            # the user can see the output.
-            print()
-            input("Press enter to continue...")
+    args = parse_args()
+    run(args)
 
 
-def parse_args(is_ci):
+def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -77,36 +55,8 @@ def parse_args(is_ci):
 def run(args):
     env.ensure_dependencies()
     env.ensure_in_venv(__file__, auto_create=True)
-
-    if not args.skip_python:
-        env.install_requirements()
-
-    colors = env.import_colors()
-
-    if args.only_python:
-        print()
-        print(colors.SUCCESS_TEXT + " Only Python dependencies installed")
-        return
-
-    try:
-        install(args)
-
-        print()
-        print(f"\n{colors.SUCCESS_TEXT} Dependencies installed")
-
-        # On Windows and macOS, we set env vars for cmake, but for them to be picked up,
-        # either the shell needs to be restarted or the env vars need to be re-sourced.
-        # Restarting the shell is easier for most people.
-        if not env.is_linux():
-            print(
-                f"{colors.WARNING_TEXT} You may need to restart your terminal "
-                "or IDE to use new env vars"
-            )
-    except Exception:
-        traceback.print_exc()
-        print()
-        print(f"\n{colors.ERROR_TEXT} Failed to install dependencies")
-        sys.exit(1)
+    env.install_requirements()
+    install(args)
 
 
 def install(args):
@@ -115,16 +65,14 @@ def install(args):
             deps = SubprojectDependencies(subproject)
             deps.install()
 
-    run_meson(
-        args.meson_install, args.meson_no_system, args.meson_static, args.build_dir
-    )
+    run_meson(args.meson_no_system, args.meson_static, "build")
 
 
 # It's a bit weird to use Meson just for installing deps, but it's a stopgap until
 # we fully switch from CMake to Meson. For the meantime, Meson will install the deps
 # so that CMake can find them easily. Once we switch to Meson, it might be possible for
 # Meson handle the deps resolution, so that we won't need to install them on the system.
-def run_meson(install, no_system_list, static_list, build_dir):
+def run_meson(no_system_list, static_list, build_dir):
     meson = meson_utils.Meson(build_dir)
     meson.setup(no_system_list, static_list)
 
@@ -133,8 +81,7 @@ def run_meson(install, no_system_list, static_list, build_dir):
     if env.is_linux():
         meson.compile()
 
-    if install:
-        meson.install()
+    meson.install()
 
 
 class SubprojectDependencies:
