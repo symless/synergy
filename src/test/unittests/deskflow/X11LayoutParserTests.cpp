5a1c750613a1a679d6fd2fd629fa70237f6f2786
Nick Bolton <nick@symless.com>
2025-09-25 13:57:47 +0100

    fix: correct spelling of convertLayotToISO to convertLayoutToISO in layoutConvertTest

--- src/test/unittests/deskflow/X11LayoutParserTests.cpp
+++ src/test/unittests/deskflow/X11LayoutParserTests.cpp
@@ -152,9 +152,9 @@ TEST(X11LayoutsParsingTests, xmlParsingIncorrectEvdevFileTest)
 
 TEST(X11LayoutsParsingTests, layoutConvertTest)
 {
-  EXPECT_EQ(X11LayoutsParser::convertLayotToISO(testDir + "/correctEvdev.xml", "us", true), "en");
-  EXPECT_EQ(X11LayoutsParser::convertLayotToISO(testDir + "/incorrectEvdev1.xml", "us", true), "");
-  EXPECT_EQ(X11LayoutsParser::convertLayotToISO(testDir + "/evdevFromFuture.xml", "us", true), "");
+  EXPECT_EQ(X11LayoutsParser::convertLayoutToISO(testDir + "/correctEvdev.xml", "us", true), "en");
+  EXPECT_EQ(X11LayoutsParser::convertLayoutToISO(testDir + "/incorrectEvdev1.xml", "us", true), "");
+  EXPECT_EQ(X11LayoutsParser::convertLayoutToISO(testDir + "/evdevFromFuture.xml", "us", true), "");
 }
 
 #endif
