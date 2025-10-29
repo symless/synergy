deskflow/deskflow

fix: client to server modifier press/release
Pressing a modifier on the client and moving to the server and releasing
it breaks the modifier on the client. This resolves the issue.

src/lib/platform/EiKeyState.cpp
@@ -309,4 +309,13 @@ void EiKeyState::updateXkbState(uint32_t keyval, bool isPressed)
   xkb_state_update_key(m_xkbState, keyval, isPressed ? XKB_KEY_DOWN : XKB_KEY_UP);
 }
 
+void EiKeyState::clearStaleModifiers()
+{
+  // Recreate the XKB state to clear stuck modifiers that happen when
+  // modifier keys are press on client and released on server
+  if (m_xkbState) {
+    xkb_state_unref(m_xkbState);
+  }
+  m_xkbState = xkb_state_new(m_xkbKeymap);
+}
 } // namespace deskflow

symless/synergy

src/lib/platform/EiKeyState.cpp
@@ -321,4 +321,13 @@ void EiKeyState::update_xkb_state(uint32_t keyval, bool is_pressed)
   xkb_state_update_key(xkb_state_, keyval, is_pressed ? XKB_KEY_DOWN : XKB_KEY_UP);
 }
 
+void EiKeyState::clearStaleModifiers()
+{
+  // Recreate the XKB state to clear stuck modifiers that happen when
+  // modifier keys are press on client and released on server
+  if (xkb_state_) {
+    xkb_state_unref(xkb_state_);
+  }
+  xkb_state_ = xkb_state_new(xkb_keymap_);
+}
 } // namespace deskflow