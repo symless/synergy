9e981b024b67b2539f46347ad886f04e0774ad45
Nick Bolton <nick@symless.com>
2026-06-11 17:11:22 +0100

    feat: update copyright years to 2026 across multiple files

--- extra/src/lib/synergy/gui/FeatureHandler.cpp
+++ extra/src/lib/synergy/gui/FeatureHandler.cpp
@@ -1,6 +1,6 @@
 /*
  * Synergy -- mouse and keyboard sharing utility
- * Copyright (C) 2025 Synergy App Ltd
+ * Copyright (C) 2025 - 2026 Synergy App Ltd
  *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- extra/src/lib/synergy/hooks/gui_hook.h
+++ extra/src/lib/synergy/hooks/gui_hook.h
@@ -1,6 +1,6 @@
 /*
  * Synergy -- mouse and keyboard sharing utility
- * Copyright (C) 2024 - 2025 Synergy App Ltd
+ * Copyright (C) 2024 - 2026 Synergy App Ltd
  *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
