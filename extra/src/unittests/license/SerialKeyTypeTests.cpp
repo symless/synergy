7d13979d64193604f32d18b7a5b1131639d4d5dc
Nick Bolton <nick@symless.com>
2026-06-17 23:04:56 +0100

    Update project references from Deskflow to Synergy in source files

--- extra/src/unittests/license/SerialKeyTypeTests.cpp
+++ extra/src/unittests/license/SerialKeyTypeTests.cpp
@@ -1,5 +1,5 @@
 /*
- * Deskflow -- mouse and keyboard sharing utility
+ * Synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2016 - 2026 Synergy App Ltd
  *
  * This package is free software; you can redistribute it and/or
